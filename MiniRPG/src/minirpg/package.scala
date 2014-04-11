7f8ef0204f459097e89366cc8b530fcb6adcaeb9
haferflocken <mooilo.ueu@gmail.com>
2014-04-11 02:30:27 -0700

    toPrettyString now aligns things even more neatly

--- MiniRPG/src/minirpg/package.scala
+++ MiniRPG/src/minirpg/package.scala
@@ -7,10 +7,20 @@ package object minirpg {
     def toIntOpt = catching(classOf[NumberFormatException]).opt(str.toInt);
   }
   
+  implicit class OpsForArray(val arr : Array[_]) {
+    
+    def mkEvenString(open : String = "", sep : String, close : String = "") : String = {
+      val strs = for (i <- arr) yield i.toString;
+      val maxLength = strs.foldLeft(0)((z : Int, s : String) => z max s.length);
+      val padded = for (s <- strs) yield s.reverse.padTo(maxLength, ' ').reverse;
+      return padded.mkString(open, sep, close);
+    }
+  }
+  
   implicit class OpsFor2DIntArray(val arr : Array[Array[Int]]) {
     
     def toPrettyString : String = {
-      val subs = for (e <- arr) yield e.mkString("  {", ", ", "}");
+      val subs = for (e <- arr) yield e.mkEvenString("  {", ", ", "}");
       return subs.mkString("{\n", "\n", "\n}");
     }
     
@@ -22,7 +32,7 @@ package object minirpg {
   implicit class OpsFor2DArray(val arr : Array[Array[_]]) {
     
     def toPrettyString : String = {
-      val subs = for (e <- arr) yield e.mkString("  {", ", ", "}");
+      val subs = for (e <- arr) yield e.mkEvenString("  {", ", ", "}");
       return subs.mkString("{\n", "\n", "\n}");
     }
     
