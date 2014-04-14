cf3dff50107404b6b27836432dcf2f493f1c1cd5
haferflocken <mooilo.ueu@gmail.com>
2014-04-13 23:26:45 -0700

    Added cyclops armor set.

---
+++ MiniRPG/src/minirpg/gear/CyclopsArmor.scala
@@ -0,0 +1,53 @@
+package minirpg.gear
+
+import minirpg.model._
+
+object CyclopsGoggles extends Gear {
+
+  val name = "Cyclops' Goggles";
+  val description = "Made by the legendary tailor and inventor, Cyclops.";
+  val slots = Vector("Head");
+  val powers = null;
+  val skillBonuses = Map(Skills.impactRes -> 6);
+    
+}
+
+object CyclopsJacket extends Gear {
+
+  val name = "Cyclops' Jacket";
+  val description = "Made by the legendary tailor and inventor, Cyclops.";
+  val slots = Vector("Torso");
+  val powers = null;
+  val skillBonuses = Map(Skills.impactRes -> 11);
+    
+}
+
+object CyclopsPants extends Gear {
+
+  val name = "Cyclops' Pants";
+  val description = "Made by the legendary tailor and inventor, Cyclops.";
+  val slots = Vector("Legs");
+  val powers = null;
+  val skillBonuses = Map(Skills.impactRes -> 8);
+    
+}
+
+object CyclopsGloves extends Gear {
+
+  val name = "Cyclops' Gloves";
+  val description = "Made by the legendary tailor and inventor, Cyclops.";
+  val slots = Vector("Hands");
+  val powers = null;
+  val skillBonuses = Map(Skills.impactRes -> 4);
+    
+}
+
+object CyclopsBoots extends Gear {
+
+  val name = "Frog's Boots";
+  val description = "Made by the legendary tailor and inventor, Cyclops.";
+  val slots = Vector("Feet");
+  val powers = null;
+  val skillBonuses = Map(Skills.impactRes -> 4);
+    
+}
