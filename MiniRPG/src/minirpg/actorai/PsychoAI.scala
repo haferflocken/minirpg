76643450c6ced7fc04e47f0421222beccae3f8da
haferflocken <mooilo.ueu@gmail.com>
2014-06-23 23:25:41 -0700

    Bugfix for PsychoAI so it doesn't chase itself.

--- MiniRPG/src/minirpg/actorai/PsychoAI.scala
+++ MiniRPG/src/minirpg/actorai/PsychoAI.scala
@@ -14,27 +14,24 @@ object PsychoAI extends ActorAI {
   
   def tick(actor : Actor, delta : Long) : Unit = {
     // Find something to be mad at.
-    var target = targetOf.getOrElse(actor, null);
-    if (target == null) {
-      nearRegion.anchorX = actor.x;
-      nearRegion.anchorY = actor.y;
-      val nearby = actor.world.getEntitiesIn(nearRegion).collect( {case a : Actor => a} );
-      if (nearby.nonEmpty) {
-        target = nearby(0);
-        targetOf(actor) = target;
-      }
+    nearRegion.anchorX = actor.x;
+    nearRegion.anchorY = actor.y;
+    if (!targetOf.contains(actor)) {
+      val nearby = actor.world.getEntitiesIn(nearRegion).collect( {case a : Actor => a} ).filter(_ != actor);
+      if (nearby.nonEmpty) 
+        targetOf(actor) = nearby(0);
+    }
+    else {
+      val target = targetOf(actor);
+      if (!nearRegion.contains(target.x, target.y))
+        targetOf -= actor;
     }
     
-    // Murder it.
+    // Go to murder town.
+    val target = targetOf.getOrElse(actor, null);
     if (target != null && !actor.hasPath && !actor.isNextTo(target.x, target.y) && actor.powers.contains(Move) && Move.canBeUsedBy(actor)) {
-      nearRegion.anchorX = actor.x;
-      nearRegion.anchorY = actor.y;
-      if (nearRegion contains (target.x, target.y)) {
-        val (x, y) = actor.world.getSpotNextTo(target.x, target.y);
-        Move(actor, null, Region.tile(x, y));
-      }
-      else
-        targetOf -= actor;
+      val (x, y) = actor.world.getSpotNextTo(target.x, target.y);
+      Move(actor, null, Region.tile(x, y));
     }
   };
 
